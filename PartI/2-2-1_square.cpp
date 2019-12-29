f6ab8e72f015aee9de8fbf5742742a8d57fca300
ynrm <b1016229@fun.ac.jp>
2019-12-29 21:38:35 +0900

    2 Samples in A Tour of C++

---
+++ PartI/2-2-1_square.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;          // make names from std visible without std::
+
+double square(double x){      // square a double precision floating-point number
+  return x*x;
+}
+
+void print_square(double x){
+  cout<<"the square of "<< x <<" is "<<square(x)<<"\n";
+}
+
+int main(){
+  print_square(1.234);        // print: the square of 1.234 is 1.52276
+}
+
